84693b5836b85e35d383a2a72f526503af5d52a1
nakaji <0619nakaji@gmail.com>
2021-12-07 11:50:53 +0900

    的、毬栗追加

---
+++ Assets/Scripts/IgaguriController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgaguriController : MonoBehaviour
+{
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Shoot(new Vector3(0, 200, 2000));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
